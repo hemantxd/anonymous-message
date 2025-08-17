adb331d837a79f96adc35fdd8bd654da10a74e75
Hemant Bisht <bishthemant169@gmail.com>
2025-08-17 20:58:44 +0530

    otp verification and unique username check

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import {z} from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+
+export async function GET(request: Request) {
+
+    //this code is not needed in newer  version of nextjs
+    // if(request.method !== "GET") {
+    //     return Response.json({ success: false, message: "Method not allowed" }, { status: 405 });
+    // }
+
+    try {
+        await dbConnect();
+        
+        const {searchParams} = new URL(request.url);
+        const queryParam = {
+            username: searchParams.get("username")
+        }
+
+
+        //validate with zod
+
+        const  result = UsernameQuerySchema.safeParse(queryParam);
+        console.log(result);
+
+        if (!result.success) {
+            //const usernameErrors = result.error.format().username?._errors || [];
+            return Response.json({ success: false, message: "Invalid username" }, { status: 400 });
+        }
+
+        const {username} = result.data;
+
+        const existingUser = await UserModel.findOne({username, isVerified: true});
+
+        if (existingUser) {
+            return Response.json({ success: false, message: "Username already exists" }, { status: 400 });
+        } else {
+            return Response.json({ success: true, message: "Username is unique" }, { status: 200 });
+        }
+
+        
+    } catch (error) {
+        return Response.json({ success: false, message: "Error checking username" }, { status: 500 });
+    }
+}
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,40 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+
+    await dbConnect();
+    try {
+        const { username, verifyCode } = await request.json();
+
+        const decodedUsername = decodeURIComponent(username);
+        const decodedVerifyCode = decodeURIComponent(verifyCode);
+        
+        const user = await UserModel.findOne({ username: decodedUsername });
+
+        if (!user) {
+            return Response.json({ success: false, message: "User not found" }, { status: 404 });
+        }
+
+        if (user.verifyCode !== decodedVerifyCode) {
+            return Response.json({ success: false, message: "Invalid verification code" }, { status: 400 });
+        }
+
+        if (user.isVerified) {
+            return Response.json({ success: false, message: "User already verified" }, { status: 400 });
+        }
+
+        if(user.verifyCodeExpiry < new Date()) {
+            return Response.json({ success: false, message: "Verification code expired" }, { status: 400 });
+        }
+
+        user.isVerified = true;
+        await user.save();
+
+        return Response.json({ success: true, message: "Verification successful" }, { status: 200 });
+    } catch (error) {
+        
+        console.log(error);
+        return Response.json({ success: false, message: "Error verifying code" }, { status: 500 });
+    }
+}
