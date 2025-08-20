90274bc2cbedc9c497a2663d05e2df9f86b87f60
Hemant Bisht <bishthemant169@gmail.com>
2025-08-20 21:45:09 +0530

    send message api

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,37 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+import { Message } from "@/model/User";
+
+export async function POST(request: Request){
+    await dbConnect();
+    const {username, content} = await request.json();
+    
+    try {
+        const user = await UserModel.findOne({username: username});
+        if(!user){
+            return Response.json({success: false, message: "User not found"}, {status: 404});
+        }
+
+        //is user accepting message
+        if(!user.isAcceptingMessage){
+            return Response.json({success: false, message: "User is not accepting messages"}, {status: 400});
+        }
+
+        const newMessage = {   
+            content: content,
+            createdAt: new Date()
+        }
+
+        user.messages.push(newMessage as Message);
+        await user.save();
+
+        return Response.json({success: true, message: "Message sent"}, {status: 200});
+
+
+    } catch (error) {
+        
+        return Response.json({success: false, message: "Error sending message"}, {status: 500});
+    }
+
+}
