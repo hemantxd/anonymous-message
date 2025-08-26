fb1f0440212bff97fd901c473ebbebb5d7a1c54d
Hemant Bisht <bishthemant169@gmail.com>
2025-08-26 18:48:59 +0530

    Navbar and Message Card UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster, toast } from 'sonner'
 import Navbar from "@/components/Navbar";
 
 
@@ -26,15 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <AuthProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        
         {children}
-      
+      <Toaster />
       </body>
-      
+      </AuthProvider>
     </html>
   );
 }
---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React from "react";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { toast } from "sonner";
+import axios from "axios";
+import { Message } from "@/model/User";
+
+type MessageCardProps = {
+    message: Message,
+    onMessageDelete: (messageId: string) => void
+}
+
+
+
+const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
+
+    const handleDeleteConfirm = async () => {
+        
+        const response = await axios.delete(`/api/delete-message/${message._id}`)
+        toast.success(response.data.message)
+        onMessageDelete(message._id)
+    }
+
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive"><X className="h-5 w-5"/></Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+     
+      
+    </Card>
+  );
+};
+
+export default MessageCard;
